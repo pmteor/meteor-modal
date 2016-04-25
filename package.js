c29e638300f7d84a06069ac9c50da0637f38a1eb
Yasar icli <hello@yasaricli.com>
2016-04-25 11:02:07 +0300

    Bump 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pmteor:modal',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Modal package for Meteor.',
   git: 'https://github.com/pmteor/meteor-modal.git',
   documentation: 'README.md'
